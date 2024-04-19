4ea3143f09006f2c91ea7b1c0921be5122215015
Roger Lopez <rllopez0410@gmail.com>
2024-04-19 02:55:22 -0400

    created the basic UI main content and also included a character count as well as a number line

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
 	import { marked } from 'marked';
+    import { writable } from 'svelte/store';
+
 	let value = `Some words are *italic*, some are **bold**\n\n- lists\n- are\n- cool`;
+    let count = writable(value.length);
+    
+    $: {
+        count.set(value.length);
+    }
 </script>
 
 <div class="container">
@@ -11,17 +18,30 @@
     </header>
 
     <main>
-        <div class="txt-input">
-            input
-            <textarea bind:value={value}></textarea>
+        <div class="editor-head">
+            <div class="left-side"><p>Markdown</p></div>
+            <div class="right-side"><p>Preview</p><p>Characters:<i style="color: black;">{$count}</i></p></div>
         </div>
-    
-        <div class="txt-output">
-            output
-            <div>{@html marked(value)}</div>
+
+        <div class="ui">
+            <div class="txt-input">
+                <div class="number-line">
+                    {#each Array.from({ length: value.split('\n').length }) as _, i}
+                        <div>{i + 1}</div>
+                    {/each}
+                </div>
+                <textarea bind:value={value}></textarea>
+            </div>
+        
+            <div class="txt-output">
+                {@html marked(value)}
+            </div>
         </div>
+
     </main>
-    <footer>footer</footer>
+    <footer>
+        footer
+    </footer>
 </div>
 
 <style>
@@ -46,7 +66,7 @@ header p {
   font-family: "Source Code Pro", monospace;
   font-size: 24px;
   overflow: hidden;
-  border-right: .15em solid orange;
+  border-right: .15em solid orangered;
   white-space: nowrap;
   margin: 0 auto;
   letter-spacing: .15em;
@@ -63,7 +83,7 @@ span {
 
 @keyframes typing {
   0% { width: 0 }
-  25% { width: 0; }
+  25% { width: 0 }
   50% { width: 750px }
   70% { width: 750px }
   75% { width: 750px }
@@ -75,11 +95,83 @@ span {
   50% { border-color: orangered }
 }
 
+
 main {
+    display: flex;
+    flex-direction: column;
     flex: 5;
+}
+
+.editor-head {
+    display: flex;
+    border-bottom: 1.5px solid rgb(215, 215, 215);
+    font-family: "Source Code Pro", monospace;
+    color: #b6b6b6;
+}
+
+.left-side {
+    flex: 1;
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+.right-side {
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+    border-left: 1.5px solid rgb(215, 215, 215);
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+.ui {
+    display: flex;
+    flex: 1;
+}
+
+.txt-input {
+    display: flex;
+    overflow: hidden;
+    flex: 1;
+}
+
+.number-line {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: .2;
+    gap: 3.09px;
+    box-shadow: .5px 0px 4px rgb(156, 156, 156);
+    z-index: 1;
+}
+
+.number-line :nth-child(n + 1) {
+    height: 16px;
+}
+
+textarea {
+    resize: none;
+    flex: 5;
+    border: none;
+    background-color: #f2f7ff;
+    font-size: 16px;
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+textarea:focus {
+    outline: none;
+}
+
+.txt-output {
+    display: flex;
+    text-align: left;
+    flex-direction: column;
+    flex: 1;
 }
 
 footer {
     flex: .2;
+    border-top: 1.5px solid rgb(215, 215, 215);
 }
 </style>
